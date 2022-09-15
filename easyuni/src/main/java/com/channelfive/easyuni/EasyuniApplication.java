6bf527a9d516b760580252a4673f1a4eef8969cc
Yong Jie Lau <yj@Yongs-Air.local>
2022-09-15 20:58:35 +0800

    Added spring boot with hibernate project files

---
+++ easyuni/src/main/java/com/channelfive/easyuni/EasyuniApplication.java
@@ -0,0 +1,13 @@
+package com.channelfive.easyuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyuniApplication.class, args);
+	}
+
+}
---
+++ easyuni/src/test/java/com/channelfive/easyuni/EasyuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channelfive.easyuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
